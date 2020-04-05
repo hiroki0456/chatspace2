95fee1966c615caa067640d65f8907f306db3aa5
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2020-04-05 14:48:20 +0900

    automatic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -73,12 +73,29 @@ $(function() {
       alert('送信に失敗しました');
     })
   });
-  let reloadmessages = function(){
+  let reloadMessages = function(){
     let last_message_id = $('.main-bar__middle__box:last').data("message-id");
     $.ajax({
       url: "api/messages",
       type: 'get',
       dataType: 'json',
+      data: {id: last_message_id}
     })
+    .done(function(messages){
+      if (messages.length !== 0) {
+      let insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main-bar__middle').append(insertHTML);
+      $('.main-bar__middle').animate({ scrollTop: $('.main-bar__middle')[0].scrollHeight});
+    }
+    })
+    .fail(function(){
+      alert('更新に失敗しました')
+    })
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
   }
 });
